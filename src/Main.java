91cba7889175bf9e9a3cdf45596e04adfa694115
Bella Khamchieva <bella.kh29@gmail.com>
2021-09-15 13:11:25 +0300

    Main.java modified

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
         public static void main(String[] args) {
         int current = 132; //вводим сумму текущего баланса
-        int addSum = 1890; //вводим сумму пополнения счета
+        int addSum = 1880; //вводим сумму пополнения счета
+        int total = addSum + current;
         int bonus = addSum/100*1;
+        int totalBonus = bonus + addSum + current;
 
         if (addSum > 1000) {
-            int total = bonus + addSum + current;
             System.out.println("Текущая сумма вашего счета составляет" + " " + total + " " + "руб." );
             System.out.println("Ваш бонус составил" + " " + bonus + " " + "руб.");
         }
         else {
-            int total = addSum + current;
             System.out.println("Текущая сумма вашего счета составляет" + " " + total + " " + "руб." );
-            System.out.println("Пополните баланс счета вашего телефона на сумму более 1000 руб. и получите бонус в 1 рубль за каждые 100 рублей!");
+
         }
     }
     }
