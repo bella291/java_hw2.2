e59171fc63b1f8fa4b34e3c5f69f44e5c9dddb82
bella291 <88541735+bella291@users.noreply.github.com>
2021-09-21 19:22:55 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,17 @@
 public class Main {
         public static void main(String[] args) {
-        int current = 132; //вводим сумму текущего баланса
-        int addSum = 1880; //вводим сумму пополнения счета
+        int current = 100; //вводим сумму текущего баланса
+        int addSum = 300; //вводим сумму пополнения счета
         int total = addSum + current;
-        int bonus = addSum/100*1;
-        int totalBonus = bonus + addSum + current;
 
         if (addSum > 1000) {
-            System.out.println("Текущая сумма вашего счета составляет" + " " + total + " " + "руб." );
+            int bonus = addSum/100;
+            int totalBonus = addSum + current + bonus;
             System.out.println("Ваш бонус составил" + " " + bonus + " " + "руб.");
+            System.out.println("Текущая сумма вашего счета составляет" + " " + totalBonus + " " + "руб." );
         }
         else {
             System.out.println("Текущая сумма вашего счета составляет" + " " + total + " " + "руб." );
-
         }
     }
     }
